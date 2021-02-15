f7c761b6dabfe039f5406dda215dbf7a9204afc1
sanghoon <ithingvv@.naver.com>
2021-02-16 03:02:09 +0900

    UserDetailService UserPermission UserRole PasswordConfig

--- project/src/main/java/com/security/project/security/ApplicationSecurityConfig.java
+++ project/src/main/java/com/security/project/security/ApplicationSecurityConfig.java
@@ -1,14 +1,25 @@
 package com.security.project.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.security.project.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,9 +27,35 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 authorizeRequests()
                 .antMatchers("/", "index","/css/*","/js/*")
                 .permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails kimUser = User.builder()
+                .username("kim")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails leeUser = User.builder()
+                .username("lee")
+                .password(passwordEncoder.encode("password"))
+                .roles(ADMIN.name())
+                .build();
+
+        UserDetails parkUser = User.builder()
+                .username("park")
+                .password(passwordEncoder.encode("password"))
+                .roles(ADMINTRAINEE.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                kimUser,leeUser,parkUser
+        );
     }
 }
